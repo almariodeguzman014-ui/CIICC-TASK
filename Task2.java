85f9375dbd4f43ebb7fe962e5fe83f310b08a41d
almariodeguzman014-ui <almario.deguzman014@gmail.com>
2025-09-18 14:12:57 +0800

    Create Task2.java
    Activity 2

---
+++ Task2.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 3;
+
+        short s = 5;
+
+        int i = 1;
+
+        char c = 'H';
+
+        float f = 2.0f;
+
+        boolean bool = true;
+
+        String output = "" + c + b + i + i + "0" + " " +"w0r1d"+" "+ f + " " + bool;
+
+        System.out.println(output);
+
+    }
+
+}
